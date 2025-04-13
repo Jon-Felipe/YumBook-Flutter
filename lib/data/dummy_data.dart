727bc1c33315fd54ce6ac7ece78c24aa4df9abca
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-13 13:51:03 +0200

    list of recipe categories

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,49 @@
+import 'package:yumbook_flutter/models/category.dart';
+
+const List<Category> categories = [
+  Category(
+    id: 'c1',
+    name: 'Breakfast',
+    description: "Start your day with a healthy and delicious breakfast.",
+  ),
+  Category(
+    id: 'c2',
+    name: 'Lunch',
+    description: "Quick and easy lunch recipes to fuel your day.",
+  ),
+  Category(
+    id: 'c3',
+    name: 'Dinner',
+    description: "Hearty and satisfying dinner ideas for the whole family.",
+  ),
+  Category(
+    id: 'c4',
+    name: 'Dessert',
+    description: "Sweet treats and indulgent desserts to end your meal.",
+  ),
+  Category(
+    id: 'c5',
+    name: 'Snacks',
+    description: "Quick and tasty snacks to enjoy throughout the day.",
+  ),
+  Category(
+    id: 'c6',
+    name: 'Gluten-Free',
+    description: "Delicious recipes for those who avoid gluten.",
+  ),
+  Category(
+    id: 'c6',
+    name: 'Vegetarian',
+    description: "Tasty vegetarian meals for a balanced diet.",
+  ),
+  Category(
+    id: 'c6',
+    name: 'Quick & Easy',
+    description: "Simple and fast recipes for busy individuals.",
+  ),
+  Category(
+    id: 'c7',
+    name: 'Budget Meals',
+    description: "Affordable recipes that don’t break the bank.",
+  ),
+];
