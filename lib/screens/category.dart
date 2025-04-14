9ca091caaf79d47f7a0692c3ac62ed4f6f233462
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-14 15:00:25 +0200

    displaying the list of categories in the category screen

--- lib/screens/category.dart
+++ lib/screens/category.dart
@@ -1,13 +1,63 @@
 import 'package:flutter/material.dart';
 
+import 'package:yumbook_flutter/data/dummy_data.dart';
+
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Categories'),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
       backgroundColor: Colors.white,
-      body: Center(child: Text('Category')),
+      body: GridView.builder(
+        padding: EdgeInsets.all(24),
+        itemCount: categories.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 15,
+          mainAxisSpacing: 15,
+          childAspectRatio: 1,
+        ),
+        itemBuilder: (ctx, index) {
+          final category = categories[index];
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.grey.shade300, width: 2),
+                    ),
+                    alignment: Alignment.center,
+                    child: Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Text(
+                        category.name,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        softWrap: true,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
