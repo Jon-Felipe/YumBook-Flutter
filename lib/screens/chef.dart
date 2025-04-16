fc9f0d6cbcffdf79732da82a7918b6d6b44315ef
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-16 18:50:51 +0200

    new chef screen init

---
+++ lib/screens/chef.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChefScreen extends StatelessWidget {
+  const ChefScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
