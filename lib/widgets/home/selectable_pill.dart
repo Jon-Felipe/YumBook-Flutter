2230a208171b934164a15b59537a0b93428a13ac
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-18 10:23:27 +0200

    selectable pill init

---
+++ lib/widgets/home/selectable_pill.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SelectablePill extends StatelessWidget {
+  const SelectablePill({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
