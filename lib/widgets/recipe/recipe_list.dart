b0cb258f3570d4d32b6c6eefecaeeedd9fc9454a
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-19 11:08:38 +0200

    recipe list horizontal conditional styling render check

--- lib/widgets/recipe/recipe_list.dart
+++ lib/widgets/recipe/recipe_list.dart
@@ -5,20 +5,25 @@ import 'package:yumbook_flutter/models/recipe.dart';
 import 'package:yumbook_flutter/widgets/recipe/recipe_card.dart';
 
 class RecipeList extends StatelessWidget {
-  const RecipeList({super.key, required this.recipes});
+  const RecipeList({
+    super.key,
+    required this.recipes,
+    this.horizontalScroll = true,
+  });
 
   final List<Recipe> recipes;
+  final bool horizontalScroll;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 220,
+      height: horizontalScroll ? 220 : double.infinity,
       child: ListView.builder(
-        scrollDirection: Axis.horizontal,
+        scrollDirection: horizontalScroll ? Axis.horizontal : Axis.vertical,
         itemCount: recipes.length,
         itemBuilder: (ctx, index) {
           final recipe = recipes[index];
-          return RecipeCard(recipe: recipe);
+          return RecipeCard(recipe: recipe, horizontalScroll: horizontalScroll);
         },
       ),
     );
