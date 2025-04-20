51af903ced9bb8a199a7fc1b0b1ad61d30102eb3
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-20 09:50:39 +0200

    wrapped recipe card with InkWell widget and navigate to recipe screen on tap

--- lib/widgets/recipe/recipe_card.dart
+++ lib/widgets/recipe/recipe_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:yumbook_flutter/models/recipe.dart';
+import 'package:yumbook_flutter/screens/recipe.dart';
 
 class RecipeCard extends StatelessWidget {
   const RecipeCard({
@@ -17,91 +18,102 @@ class RecipeCard extends StatelessWidget {
     return SizedBox(
       width: horizontalScroll ? 300 : double.infinity,
       height: horizontalScroll ? null : 250,
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-        clipBehavior: Clip.antiAlias,
-        child: Stack(
-          children: [
-            Positioned.fill(
-              child: Image.network(
-                'https://images.unsplash.com/photo-1608219992759-8d74ed8d76eb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-                fit: BoxFit.cover,
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (ctx) => RecipeScreen()),
+          );
+        },
+        borderRadius: BorderRadius.circular(16),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          clipBehavior: Clip.antiAlias,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: Image.network(
+                  'https://images.unsplash.com/photo-1608219992759-8d74ed8d76eb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                color: Colors.grey.shade100,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      recipe.name,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Row(
-                      spacing: 5,
-                      children: [
-                        Icon(
-                          FontAwesomeIcons.clock,
-                          size: 14,
-                          color: Colors.orange,
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  color: Colors.grey.shade100,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        recipe.name,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
                         ),
-                        Text('${recipe.totalTimeMinutes} min'),
-                        Text('• ${recipe.difficulty.difficultyString}'),
-                        Text('• by ${recipe.author}'),
-                      ],
-                    ),
-                  ],
+                      ),
+                      Row(
+                        spacing: 5,
+                        children: [
+                          Icon(
+                            FontAwesomeIcons.clock,
+                            size: 14,
+                            color: Colors.orange,
+                          ),
+                          Text('${recipe.totalTimeMinutes} min'),
+                          Text('• ${recipe.difficulty.difficultyString}'),
+                          Text('• by ${recipe.author}'),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Positioned(
-              top: 10,
-              left: 10,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  color: Colors.white,
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                child: Row(
-                  spacing: 5,
-                  children: [
-                    Icon(
-                      FontAwesomeIcons.solidStar,
-                      color: Colors.amber,
-                      size: 14,
-                    ),
-                    Text(recipe.averageRating.toString()),
-                    Text('(${recipe.numberOfReviews.toString()} Reviews)'),
-                  ],
-                ),
-              ),
-            ),
-            Positioned(
-              top: 10,
-              right: 10,
-              child: Container(
-                width: 40,
-                height: 40,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: IconButton(
-                  onPressed: () {},
-                  icon: Icon(FontAwesomeIcons.heart, size: 20),
+              Positioned(
+                top: 10,
+                left: 10,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.white,
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                  child: Row(
+                    spacing: 5,
+                    children: [
+                      Icon(
+                        FontAwesomeIcons.solidStar,
+                        color: Colors.amber,
+                        size: 14,
+                      ),
+                      Text(recipe.averageRating.toString()),
+                      Text('(${recipe.numberOfReviews.toString()} Reviews)'),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+              Positioned(
+                top: 10,
+                right: 10,
+                child: Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(FontAwesomeIcons.heart, size: 20),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
