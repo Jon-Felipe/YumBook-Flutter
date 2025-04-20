3660ef807411fcd5bbcb24e532af1b79b6061ade
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-20 09:50:07 +0200

    recipe screen init

---
+++ lib/screens/recipe.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RecipeScreen extends StatelessWidget {
+  const RecipeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Recipe Screen'),
+        backgroundColor: Colors.white,
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
